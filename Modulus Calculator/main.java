28d48a051b492163d09308556e02685c82a49a90
omsrisainathayanamaha <57611122+omsrisainathayanamaha@users.noreply.github.com>
2020-05-16 20:35:34 -0700

    Add files via upload

---
+++ Modulus Calculator/main.java
@@ -0,0 +1,24 @@
+import java.lang.System;
+import java.util.Scanner; 
+import java.io.Math;
+import java.util.Math;
+class main{
+    
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Please enter two integers in the following textboxes.");
+        int firstNumber = scan.nextInt();
+        int secondNumber = scan.nextInt();
+        int x = firstNumber;
+        int y = secondNumber;
+        int modulus = x%y;
+        System.out.println("Quotient without decimal point: "+(x+y)+". Modulus: "+modulus+".");
+
+        
+        
+    }
+        
+    
+}
